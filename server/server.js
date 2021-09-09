bfe28b2b5abdbe833daa22d08ee3a7ed2b9a8cba
Siamak Alipour <siamakalipour37@gmail.com>
2021-09-09 19:11:30 +0430

    env added + some files

---
+++ server/server.js
@@ -0,0 +1,22 @@
+// imports modules
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+// App config
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// middlewares
+app.use(express.json())
+
+// DB Config
+
+// API Endpoints
+app.get('/', (req, res) => {
+	res.send('hell')
+})
+// listener
+app.listen(PORT, () => {
+	console.log(`running on port ${PORT}`)
+})
