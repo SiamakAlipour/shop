f224a2f6187d07f3096e07b14b2568295b31a197
Siamak Alipour <siamakalipour37@gmail.com>
2021-09-12 23:47:36 +0430

    added to Feed part  + content part to Main

--- src/container/Feed.js
+++ src/container/Feed.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Product from '../components/Product'
 import './styles/Feed.scss'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
 function Feed() {
+	const [catValue, setCatValue] = useState('0')
+	const handleCatValue = (e) => {
+		setCatValue(e.currentTarget.attributes['data-value'].value)
+	}
 	return (
 		<div className='feed'>
-			<h1>Feed</h1>
+			<div className='feed__header'>
+				<h3>محصولات</h3>
+				<div className='feed__category'>
+					<InputLabel id='label' style={{ fontFamily: 'IRANSans' }}>
+						دسته بندی
+					</InputLabel>
+					<Select
+						labelId='label'
+						style={{ fontFamily: 'IRANSans' }}
+						id='select'
+						value={catValue}>
+						<MenuItem
+							style={{ fontFamily: 'IRANSans' }}
+							value='0'
+							onClick={handleCatValue}>
+							تازه ترین ها
+						</MenuItem>
+						<MenuItem
+							style={{ fontFamily: 'IRANSans' }}
+							value='1'
+							onClick={handleCatValue}>
+							Twenty
+						</MenuItem>
+					</Select>
+				</div>
+			</div>
+			<div className='feed__products'>
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+			</div>
 		</div>
 	)
 }
