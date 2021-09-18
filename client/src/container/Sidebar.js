713ea021c901a220478d08e58e0a5b94aef376c5
Siamak Alipour <siamakalipour37@gmail.com>
2021-09-18 17:59:59 +0430

    sidebar widgets added

--- client/src/container/Sidebar.js
+++ client/src/container/Sidebar.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Widget from '../components/Widget'
 import './styles/Sidebar.scss'
+import TelegramIcon from '@mui/icons-material/Telegram'
+import InstagramIcon from '@mui/icons-material/Instagram'
 function Sidebar() {
+	const socialIcons = (
+		<div className='widget__content'>
+			<p>ما را در شبکه های اجتماعی دنبال کنید</p>
+			<div className='widget__icons'>
+				<a href='#'>
+					<img
+						className='widget__icons--style'
+						src='https://www.freepnglogos.com/uploads/telegram-logo-2.png'
+						alt=''
+					/>
+				</a>
+				<a href='#'>
+					<img
+						className='widget__icons--style'
+						src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png'
+						alt=''
+					/>
+				</a>
+			</div>
+		</div>
+	)
+
 	return (
 		<div className='sidebar'>
-			<Widget />
-			<Widget />
-			<Widget />
+			<Widget title={'شبکه های اجتماعی'} content={socialIcons} />
 		</div>
 	)
 }
--- client/src/store/reducers/productReducer.js
+++ client/src/store/reducers/productReducer.js
@@ -6,7 +6,22 @@ import {
 	PRODUCT_GET,
 } from '../actions/types'
 import { v4 } from 'uuid'
-const initialState = { items: [] }
+const initialState = {
+	items: [
+		{
+			name: 'product 1',
+			description: 'best product 1',
+			image: 'https://picsum.photos/300/300',
+			price: 25000,
+		},
+		{
+			name: 'product 2',
+			description: 'best product 2',
+			image: 'https://picsum.photos/301/300',
+			price: 50000,
+		},
+	],
+}
 const productReducer = (state = initialState, action) => {
 	const { type, payload } = action
 	switch (type) {
