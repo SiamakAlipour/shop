ce91371d1fbda2c80dde816e27ee52b883928959
Siamak Alipour <siamakalipour37@gmail.com>
2021-09-18 09:01:28 +0430

    widgets component added + some style bug fixed

---
+++ client/src/components/Widget.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/Widget.scss'
+function Widget() {
+	return (
+		<div className='widget'>
+			<h3 className='widget__name'>widget name</h3>
+			<div className='widget__content'></div>
+		</div>
+	)
+}
+
+export default Widget
