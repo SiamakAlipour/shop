6da2952be5d755d6fcda7da3e0a72e506232141e
Siamak Alipour <siamakalipour37@gmail.com>
2021-09-24 17:57:03 +0330

    account part added + add product ui

---
+++ src/container/Account.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './styles/Account.scss'
+import Option from './Option'
+import { Link } from 'react-router-dom'
+
+import './styles/Account.scss'
+function Account() {
+	const [option, setOption] = useState(0)
+	const [admin, setAdmin] = useState(true)
+	React.useEffect(() => {
+		console.log(option)
+	})
+	return (
+		<div className='account'>
+			<div className='account__sideOptions'>
+				<ul>
+					<Link to='/'>
+						<li>خانه</li>
+					</Link>
+
+					{admin ? (
+						<li onClick={() => setOption(0)}>اضافه کردن کالا</li>
+					) : null}
+					{admin ? (
+						<li onClick={() => setOption(1)}>مدیریت کاربران</li>
+					) : null}
+					<li onClick={() => setOption(2)}>سبد خرید</li>
+					<li onClick={() => setOption(3)}>ویرایش حساب کاربری</li>
+				</ul>
+			</div>
+			<div className='account__content'>
+				<Option option={option} />
+			</div>
+		</div>
+	)
+}
+
+export default Account
