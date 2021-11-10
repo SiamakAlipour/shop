4ff3d096a2d14a76983f41b0e41c5c95fc4df73e
Siamak Alipour <siamakalipour37@gmail.com>
2021-11-10 17:16:31 +0330

    search part component added

---
+++ client/src/container/SearchPart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/SearchPart.scss';
+function SearchPart() {
+	return (
+		<div className='searchPart'>
+			<h1>this is Search Part</h1>
+		</div>
+	);
+}
+
+export default SearchPart;
